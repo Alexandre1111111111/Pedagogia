dcd0e0023d01fde41d0c82be549cf79acf05885f
Nenoplayhard52 <125273548+Nenoplayhards2@users.noreply.github.com>
2024-10-22 09:03:46 -0300

    Add files via upload

--- index.php
+++ index.php
@@ -1,10 +1,10 @@
 <?php
+    include("banco.php");
     session_start();
 ?>
 <!DOCTYPE html>
-<html lang="pt-BR">
+<html lang="pt-br">
 <head>
-    <meta name="google" content="notranslate">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Sistema</title>
@@ -12,6 +12,13 @@
     <link rel="Icon" href="">
 </head>
 <body>
-
+    <form action=>
+      <p>Faça login</p>
+       <label for="Cpf">CPF<input type="text" id="cpf"/></label>
+       <label for="Senha">Senha<input type="password" id="senha"/></label>
+       <button>Continuar</button>
+        <a href="esqueci.html">Esqueci minha senha</a>
+        <a href="Criar.html">Não tenho conta!</a>
+    </form>
 </body>
 </html>
